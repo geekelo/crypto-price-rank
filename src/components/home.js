58b5913b1a9150ee5d65f11d8ff7e3fd3427f100
geekelo <efuelight12@gmail.com>
2023-08-16 10:57:51 +0100

    Fix linter errors

--- src/components/home.js
+++ src/components/home.js
@@ -1,16 +1,16 @@
 function Home() {
-  return(
+  return (
     <section>
       <div className="landingSection">
-    
+        ppp
       </div>
       <main>
         <ul>
-
+        ppp
         </ul>
       </main>
     </section>
-  )
+  );
 }
 
 export default Home;
